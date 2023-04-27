f7dcb0d8af28c718dd68c792d25e9b68e7ae8ebd
Artem Androsov <androsov.artem.v@gmail.com>
2023-04-27 20:23:19 +1100

    serializer_2

--- backend/foodgram/app/admin.py
+++ backend/foodgram/app/admin.py
@@ -12,9 +12,60 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import BaseUserManager
 from users.models import CustomUser
 
+# class CustomUserManager(BaseUserManager):
+#     def create_user(self, email, username, password, fitst_name, last_name):
+#         if not email:
+#             raise ValueError("Users must have an email address")
 
-class UsersModelAdmin(admin.ModelAdmin):
-    list_display = ("username", "pk", "first_name", "last_name", "email", "password")
+#         user = self.model(
+#             email=self.normalize_email(email),
+#             date_of_birth=date_of_birth,
+#         )
+
+#         user.set_password(password)
+#         user.save(using=self._db)
+#         return user
+
+#     def create_superuser(
+#         self,
+#         email,
+#         username,
+#         password,
+#         fitst_name,
+#         last_name,
+#     ):
+#         """
+#         Creates and saves a superuser with the given email, date of
+#         birth and password.
+#         """
+#         user = self.create_user(
+#             email,
+#             password=password,
+#             date_of_birth=date_of_birth,
+#         )
+#         user.is_admin = True
+#         user.save(using=self._db)
+#         return user
+
+
+class UsersModelAdmin(UserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "password1",
+                    "password2",
+                    "first_name",
+                    "last_name",
+                    "email",
+                ),
+            },
+        ),
+    )
+    list_display = ("username", "pk", "first_name", "last_name", "email")
     list_filter = ("email", "username")
     search_fields = ("email", "username")
     empty_value_display = "-пусто-"
